95a4195c33990a0c2d26fb0d17382eca766f2851
Christiaan <chris241@hotmail.nl>
2017-08-11 18:47:24 +0200

    Auto stash before merge of "Dev" and "origin/Dev"

--- RPG-Game/Assets/Scripts/Game/PlayerController.cs
+++ RPG-Game/Assets/Scripts/Game/PlayerController.cs
@@ -2,15 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Game {
-    public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    Vector3 Accelaration;
+    Vector3 CameraAccelaration;
 
-        Vector3 Accelaration;
+    public float velocity = 5;
+    CharacterController modelController;
 
-        public void Start() { Network.InitSocket( "quikers.xyz", 23000 ); }
+    public Camera Camera;
+    public GameObject Model;
 
-        public void OnApplicationQuit() { Data.Network.Socket.Close(); }
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Network.InitSocket( "quikers.xyz", 23000 );
+        modelController = GetComponent<CharacterController>();
+    }
 
-        public void Update() { }
+    public void OnApplicationQuit() { Network.Socket.Close(); }
+
+    void Update()
+    {
+        ModelMovement();
+
+        CameraRotation();
+    }
+
+    void ModelMovement()
+    {
+        Accelaration = Vector3.zero;
+
+        Accelaration.x = Input.GetAxis( "Horizontal" ) * velocity;
+        Accelaration.z = Input.GetAxis( "Vertical" ) * velocity;
+
+        Accelaration = transform.rotation * Accelaration;
+
+        modelController.Move( Accelaration * Time.deltaTime );
+    }
+
+    void CameraRotation()
+    {
+        CameraAccelaration = Vector3.zero;
+
+        CameraAccelaration.x = Input.GetAxis( "Camera Horizontal" );
+        CameraAccelaration.y = Input.GetAxis( "Camera Vertical" );
+
+
+
+        //Debug.Log( CameraAccelaration );
     }
 }
