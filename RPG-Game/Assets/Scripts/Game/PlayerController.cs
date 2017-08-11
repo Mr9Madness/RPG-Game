2fc0f1f6e810179e820cf1b6a6d5d06973022ce4
Christiaan <chris241@hotmail.nl>
2017-08-11 16:26:46 +0200

    Auto stash before merge of "Dev" and "origin/Dev"

--- RPG-Game/Assets/Scripts/Game/PlayerController.cs
+++ RPG-Game/Assets/Scripts/Game/PlayerController.cs
@@ -8,7 +8,19 @@ public class PlayerController : MonoBehaviour
 
     public void OnApplicationQuit() { Network.Socket.Close(); }
 
-    public void Update() {
+    void Update()
+    {
+        if ( Input.GetKey( KeyCode.A ) )
+            Accelaration.x = 1;
+
+        if (Input.GetKey( KeyCode.D ))
+            Accelaration.x = -1;
+
+        if (Input.GetKey( KeyCode.W ))
+            Accelaration.z = 1;
+
+        if (Input.GetKey( KeyCode.S ))
+            Accelaration.x = -1;
 
     }
 
