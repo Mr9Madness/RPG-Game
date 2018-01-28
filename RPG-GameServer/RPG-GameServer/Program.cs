05d81c51b106b4b165e077bafb74cb3851007196
Quikers <flipmil3@hotmail.com>
2018-01-28 22:36:21 +0100

    Made some standard crap

---
+++ RPG-GameServer/RPG-GameServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_GameServer {
+
+    public class Program {
+
+        private Socket _receiver;
+        private Socket _sender;
+
+        public static void Main( string[] args ) {
+
+        }
+
+    }
+
+}
